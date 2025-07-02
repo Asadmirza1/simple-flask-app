baca13423e5e2b6d56f41998f2afac2bda92cafe
AsadAliAkhtar321 <asadaliakhtar321@gmail.com>
2025-07-02 13:43:25 +0500

    fixing the full app

--- db.py
+++ db.py
@@ -3,9 +3,9 @@ import os
 
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.environ.get("mysql.railway.internal"),
-        user=os.environ.get("root"),
-        password=os.environ.get("FYWQTURfpVkVfWBfgWIydXmylFCdgqIb"),
-        database=os.environ.get("railway"),
+        host=os.environ.get("MYSQLHOST"),
+        user=os.environ.get("MYSQLUSER"),
+        password=os.environ.get("MYSQLPASSWORD"),
+        database=os.environ.get("MYSQLDATABASE"),
         port=int(os.environ.get("MYSQLPORT", 3306))
     )
