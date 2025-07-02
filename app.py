18358bb590a04f07e221235374a51517a9e69397
AsadAliAkhtar321 <asadaliakhtar321@gmail.com>
2025-07-02 11:14:48 +0500

    Initial simple Flask app

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Hello from Railway Flask App!</h1>"
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
